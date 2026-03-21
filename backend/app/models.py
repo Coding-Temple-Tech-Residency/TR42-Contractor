53224ac9efe82b6595796972ce187e0c2f06f137
ncespedes1 <ncespedes.art@gmail.com>
2026-03-20 20:26:17 -0400

    post meeting model fixes

--- backend/app/models.py
+++ backend/app/models.py
@@ -15,10 +15,7 @@ class Auth_users(Base):
     email: Mapped[str] = mapped_column(String(360), nullable=False, unique=True)
     username: Mapped[str] = mapped_column(String(360), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(500), nullable=False)
-    role: Mapped[str] = mapped_column(String(360), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
-    offline_pin: Mapped[str] = mapped_column(String(10), nullable=True)
-
+    # created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
 
 
 class Contractors(Base):
@@ -26,19 +23,24 @@ class Contractors(Base):
 
     id: Mapped[int] = mapped_column(ForeignKey('auth_users.id'), primary_key = True, nullable=False)
     vendor_id: Mapped[int] = mapped_column(ForeignKey('vendors.id'), nullable=False)
-    vendor_manager_id: Mapped[int] = mapped_column(ForeignKey('auth_users.id'), nullable=False)
+    manager_id: Mapped[int] = mapped_column(ForeignKey('auth_users.id'), nullable=False)
     first_name: Mapped[str] = mapped_column(String(360), nullable=False)
     last_name: Mapped[str] = mapped_column(String(360), nullable=False)
     contact_number: Mapped[str] = mapped_column(String(20), nullable=False)
     date_of_birth: Mapped[date] = mapped_column(Date, nullable=False)
     address: Mapped[str] = mapped_column(String(500), nullable=False)
+    offline_pin: Mapped[str] = mapped_column(String(10), nullable=True)
+    #back up measure to reset pin.  (security questions or phone pin/phone biometrics) 
+    # send email to manager notifying new pin reset
+
+    # role: Mapped[str] = mapped_column(String(360), nullable=False) manager, worker, admin
+
 
 class Work_orders(Base):
     __tablename__ = 'work_orders'
 
     id: Mapped[int] = mapped_column(primary_key = True)
     assigned_vendor: Mapped[int] = mapped_column(ForeignKey('vendors.id'), nullable=False)
-    assigned_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     description: Mapped[str] = mapped_column(String(500), nullable=False)
     due_date: Mapped[date] = mapped_column(Date, nullable=False)
@@ -47,7 +49,8 @@ class Work_orders(Base):
     estimated_cost: Mapped[float] = mapped_column(Float, nullable=False)
     estimated_duration: Mapped[float] = mapped_column(Float, nullable=False)
     priority: Mapped[str] = mapped_column(String(360), nullable=False)
-    # assigned_contractor: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    # assigned_contractor: Mapped[int] = mapped_column(ForeignKey('contractors.id'), nullable=True) list of contractor that accepted.
+
 
 class Tasks(Base):
     __tablename__ = 'tasks'
@@ -59,14 +62,17 @@ class Tasks(Base):
     task_status: Mapped[str] = mapped_column(String(360), nullable=False)
 
     assigned_contractor: Mapped[int] = mapped_column(ForeignKey('contractors.id'))
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    contractor_assigned_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    task_created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     start_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     end_time: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     start_location: Mapped[str] = mapped_column(String(500))
     end_location: Mapped[str] = mapped_column(String(500))
-    # estimated_quantity: Mapped[float] = mapped_column(Float)
-    # unit: Mapped[str] = mapped_column(String(360))
+
+    estimated_quantity: Mapped[float] = mapped_column(Float)
+    unit: Mapped[str] = mapped_column(String(360))
     special_requirements: Mapped[str] = mapped_column(String(500))
+    
     contractor_notes: Mapped[str] = mapped_column(String(500))
     anomaly_flag: Mapped[bool] = mapped_column(Boolean, default=False)
     anomaly_reason: Mapped[str] = mapped_column(String(500))
