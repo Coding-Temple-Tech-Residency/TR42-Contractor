02e71925ee7af6446aebfddce14650db6bb2c19c
Jonathanch2022 <admin@JCH-LPT2>
2026-04-29 06:42:21 -0400

    added vendor id to demo users for filtering contact data

--- frontend/field-force-contractor/contexts/AppContext.tsx
+++ frontend/field-force-contractor/contexts/AppContext.tsx
@@ -10,7 +10,8 @@ export type userTable = {
     firstName:string,
     lastName:string,
     phone:string,
-    role:string
+    role:string,
+    vendorid:string
 }
 //Demo User Data
 export const demoUsers:userTable[] = [
@@ -19,28 +20,32 @@ export const demoUsers:userTable[] = [
     firstName:"John",
     lastName:"Doe",
     phone:"555-555-5555",
-    role:"contractor"
+    role:"contractor",
+    vendorid:"1"
  },
  {
     userid:"1",
     firstName:"Jane",
     lastName:"Doe",
     phone:"666-555-5555",
-    role:"client"
+    role:"client",
+    vendorid:""
  },
  {
     userid:"2",
     firstName:"Taylor",
     lastName:"Swith",
     phone:"777-555-5555",
-    role:"vendor"
+    role:"vendor",
+    vendorid:""
  },
  {
     userid:"3",
     firstName:"Ben",
     lastName:"Joe",
     phone:"888-555-5555",
-    role:"contractor"
+    role:"contractor",
+    vendorid:"1"
  }
 
 ]
